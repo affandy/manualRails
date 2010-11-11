48aa44843d9ce914d947a0007fb6eac6f789804d
Yanuar Affandy <affandy_89@yahoo.com>
2010-11-12 01:14:31 +0800

    User signup complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :user
+  map.resources :users
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
   map.root :controller => 'pages', :action => 'home'
-  map.signup '/signup',   :controller => 'user', :action => 'new'
+  map.signup '/signup',   :controller => 'users', :action => 'new'
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
--- spec/integration/layout_links_spec.rb
+++ spec/integration/layout_links_spec.rb
@@ -23,7 +23,7 @@ describe "LayoutLinks" do
 
   it "should have a signup page at '/signup'" do
     get '/signup'
-    response.should render_template('user/new')
+    response.should render_template('users/new')
   end
 
   it "should have the right links on the layout" do
@@ -37,6 +37,6 @@ describe "LayoutLinks" do
     click_link "Home"
     response.should render_template('pages/home')
     click_link "Sign up now!"
-    response.should render_template('user/new')
+    response.should render_template('users/new')
   end
 end
